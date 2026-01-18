19d1af4203faa3a6efb5b4bfa0074596d49c7ea8
victorhf220 <160761802+victorhf220@users.noreply.github.com>
2026-01-18 11:03:04 -0300

    Create database configuration file

---
+++ src/config/database.config.ts
@@ -0,0 +1,11 @@
+// Database Configuration
+
+const dbConfig = {
+  host: 'localhost',
+  port: 5432,
+  username: 'your_username',
+  password: 'your_password',
+  database: 'your_database',
+};
+
+export default dbConfig;
